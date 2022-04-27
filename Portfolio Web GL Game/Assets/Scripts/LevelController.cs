8a4001f9187844feb1d7f4c4006dc3efaee9a70a
John Miele <mielejohn4@gmail.com>
2022-04-26 23:42:43 -0400

    Player can spawn

---
+++ Portfolio Web GL Game/Assets/Scripts/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject PlayerSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Player, PlayerSpawn.transform.position, PlayerSpawn.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
